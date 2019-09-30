f8b4e9ee4d40e2a8eba544d13f401b65977ec972
vinay-wi <vinay.kp@weinvest.net>
2019-09-30 16:03:25 +0530

    rubocop changes

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,36 +1,34 @@
+# frozen_string_literal: true
+
 class NotesController < ApplicationController
-	def index
-		@user_notes = Note.includes(:tags)
-		  "jdfksdfkdj"
-	"1234"
-	
-	end
+  def index
+    @user_notes = Note.includes(:tags)
+  end
 
-	def new
-		user = User.first
-		@note = Note.new(user_id: user.id)
-	end
+  def new
+    user = User.first
+    @note = Note.new(user_id: user.id)
+  end
 
-	def create
-		tags = params[:tags][:ids].reject(&:blank?).map(&:to_i)
-		@note = Note.new(note_params.merge(tag_ids: tags))
-		if @note.save
-      		redirect_to notes_path
-	    else
-	      render :new
-	    end
-	end
+  def create
+    tags = params[:tags][:ids].reject(&:blank?).map(&:to_i)
+    @note = Note.new(note_params.merge(tag_ids: tags))
+    if @note.save
+      redirect_to notes_path
+    else
+      render :new
+    end
+  end
 
-	def show
-		@note = Note.includes(:tags).where("notes.id = ?", params[:id]).first
-	end
+  def show
+    @note = Note.includes(:tags).where('notes.id = ?', params[:id]).first
+  end
 
-	private
+  private
 
-	def note_params
-		params.require(:note).permit(:title, :body, :tags,:user_id)
-	end
+  def note_params
+    params.require(:note).permit(:title, :body, :tags, :user_id)
+  end
 
-	def params_edit
-	end
+  def params_edit; end
 end
